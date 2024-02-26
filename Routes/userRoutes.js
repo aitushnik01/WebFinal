ea0aed231b4c5e8eb4bb78381b5a5a04cceb1cff
MUIT IS DEAD <zhakasr14@gmail.com>
2024-02-26 10:24:51 +0600

    Register and Login

---
+++ Routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const userController = require('../Controllers/userController');
+
+// Register a new user
+router.post('/register', [
+    check('username').isLength({ min: 3 }),
+    check('email').isEmail(),
+    check('password').isLength({ min: 6 }),
+], userController.register);
+
+// Login
+router.post('/login', userController.login);
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger/swagger.json')
-const bookRoutes = require('./routes/bookRoutes')
 
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+const bookRoutes = require('./routes/bookRoutes');
+const userRoutes = require('./routes/userRoutes'); // Import user routes
 
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Swagger UI setup
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Book routes
-app.use('/books', bookRoutes)
+app.use('/books', bookRoutes);
+
+// User routes
+app.use('/users', userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
+
