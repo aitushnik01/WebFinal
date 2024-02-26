b4d8449abc9c932827538abc58fd4e852a685ce5
Diasisenov1 <dias.isen@mail.ru>
2024-02-27 00:48:44 +0600

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -16,22 +16,7 @@ const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-=======
->>>>>>> ce346dfd329540c1e6701c15d9b1a10ca5b8d8cf
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./Swagger/swagger.json')
-const bookRoutes = require('./routes/bookRoutes')
 
-const app = express()
-const port = 3000
-
-app.use(cors())
-app.use(bodyParser.json())
-
-<<<<<<< HEAD
 
 // Swagger UI setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -39,31 +24,15 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(passport.initialize());
 
-// VK routes
-app.use('/vk', vkRoutes);
-
 // Book routes
 app.use('/books', bookRoutes);
 
 // User routes
 app.use('/users', userRoutes);
 
+// VK routes
+app.use('/vk', vkRoutes)
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-=======
-// Swagger UI setup
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
->>>>>>> ce346dfd329540c1e6701c15d9b1a10ca5b8d8cf
-// Book routes
-app.use('/books', bookRoutes)
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-<<<<<<< HEAD
-
-=======
->>>>>>> ce346dfd329540c1e6701c15d9b1a10ca5b8d8cf
